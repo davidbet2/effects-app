5135dcdc9e176787e1288699b8ef6530b5a18215
david betancur jaramillo <david.betancur@pragma.com.co>
2020-08-23 13:22:24 -0500

    first commit

---
+++ src/app/services/usuario.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuarioService {
+
+  constructor( private http: HttpClient) { }
+
+  url = 'https://reqres.in/api';
+
+  getUsers () {
+    return this.http.get(`${this.url}/users?per_page=6`).pipe(
+      map( (usuario:any) => {
+      return  usuario.data
+      })
+    )
+  }
+
+}
